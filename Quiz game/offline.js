fb63f96c9bde0a53e71d6d9e4762372a9ae66504
porlin13 <polin-po13@hotmail.com>
2019-10-20 15:59:02 +0700

    xxx
    first

---
+++ Quiz game/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1571561440,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/answerimage-sheet0.png",
+		"images/answerimage2-sheet0.png",
+		"images/answerimage3-sheet0.png",
+		"images/answerimage4-sheet0.png",
+		"images/blog-sheet0.png",
+		"images/player1-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bplay-sheet0.png",
+		"images/play-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"qq.json"
+	]
+}
